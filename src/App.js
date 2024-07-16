7b0fd8b34442f883f0b9212a060348cc59a08930
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-07-16 20:27:45 -0300

    feat: add Cronometro component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import Cronometro from './components/cronometro';
 
 function App() {
+  const targetDate = new Date();
+  targetDate.setDate(targetDate.getDate() + 29); // 2 dias a partir de hoje
+
   return (
-    <>
-      <h1>Sinfcor</h1>
-    </>
+    <div className="App">
+      <h1>Cronômetro em React</h1>
+      <Cronometro targetDate={targetDate} />
+    </div>
   );
 }
 
